f796780135212ba7aa6d0174c7d4596c478a2c62
qiufeng <991564110@qq.com>
2017-12-08 13:46:30 +0800

    修改

--- MessageHandler.php
+++ MessageHandler.php
@@ -37,33 +37,12 @@ class MessageHandler
         TextType::messageHandler($message, $friends, $groups);
         RecallType::messageHandler($message);
 
-        if ($message['type'] === 'new_friend') {
-            Text::send($message['from']['UserName'], '客官，等你很久了！感谢跟 vbot 交朋友，如果可以帮我点个star，谢谢了！https://github.com/HanSon/vbot');
-            $groups->addMember($groups->getUsernameByNickname('Vbot 体验群'), $message['from']['UserName']);
-            Text::send($message['from']['UserName'], '现在拉你进去vbot的测试群，进去后为了避免轰炸记得设置免骚扰哦！如果被不小心踢出群，跟我说声“拉我”我就会拉你进群的了。');
-        }
-
-        if ($message['type'] === 'emoticon' && random_int(0, 1)) {
-            Emoticon::sendRandom($message['from']['UserName']);
-        }
 
         // @todo
         if ($message['type'] === 'official') {
             vbot('console')->log('收到公众号消息:'.$message['title'].$message['description'].
                 $message['app'].$message['url']);
         }
-        if ($message['type'] === 'red_packet') {
-            Text::send($message['from']['UserName'], "谢谢");
-        }
-        if ($message['type'] === 'text') {
-            Text::send($message['from']['UserName'],  $message['pure']);
-        }
 
-        if ($message['type'] === 'request_friend') {
-            vbot('console')->log('收到好友申请:'.$message['info']['Content'].$message['avatar']);
-            if (in_array($message['info']['Content'], ['echo', 'print_r', 'var_dump', 'print'])) {
-                $friends->approve($message);
-            }
-        }
     }
 }
